87d70eec7db86de5b5ad63fd15ffc09553c87d34
MatrixTech <developers@matrix.gameolot.info>
2014-06-23 15:41:17 -0400

    Added stylesheets + links to them
    Added:
    main.css
    normalize.css
    And links to them in index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 <head>
+<link href="fonts/stylesheet.css" rel="stylesheet" type="text/css">
+<link href="css/normalize.css" rel="stylesheet" type="text/css">
+<link href="css/main.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 </body>
