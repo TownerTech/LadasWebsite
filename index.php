3c0b1b23376fba12f19803900d603b525a2449e6
jzdev <jzdev@outlook.com>
2014-06-23 15:48:37 -0400

    added title and formatting

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
-<head>
-<link href="fonts/stylesheet.css" rel="stylesheet" type="text/css">
-<link href="css/normalize.css" rel="stylesheet" type="text/css">
-<link href="css/main.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-</body>
+	<head>
+		<link href="fonts/stylesheet.css" rel="stylesheet" type="text/css">
+		<link href="css/normalize.css" rel="stylesheet" type="text/css">
+		<link href="css/main.css" rel="stylesheet" type="text/css">
+		<title>Ladas Law Firm</title>
+	</head>
+	<body>
+	</body>
 </html>
