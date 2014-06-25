d74b9d1c7e26c9d87ab8b4a28cf75d2c00106e32
MatrixTech <developers@matrix.gameolot.info>
2014-06-25 14:12:09 -0400

    Added header and nav bar outline

--- index.php
+++ index.php
@@ -6,5 +6,18 @@
 		<title>Ladas Law Firm</title>
 	</head>
 	<body>
+	<div id="smediabar"></div>
+	<div id="header">
+	<div class="container">
+	<div id="titlewrapper">
+	<h1 id="title">Ladas Law Firm</h1>
+	</div>
+	<div id="topcontact">
+	<h2>1 (000)-000-0000</h2>
+	</div>
+	</div>
+	</div>
+	<div id="nav">
+	</div>
 	</body>
 </html>
